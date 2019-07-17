082a3a63c844d9eec5f3033c989cb2a062fd8c99
Thorsten Schifferdecker <ts@systs.org>
2019-07-17 06:08:26 +0200

    add the missing rolebinding and sync with README.md

--- deploy_kubeinvaders.sh
+++ deploy_kubeinvaders.sh
@@ -1,16 +1,19 @@
 #Change with the namespace you want to stress
 TARGET_NAMESPACE='foobar'
 
-#Change with the URL of your Kubeinvaders 
+#Change with the URL of your Kubeinvaders
 ROUTE_HOST=kubeinvaders.org
 
 kubectl apply -f kubernetes/kubeinvaders-namespace.yml
 kubectl apply -f kubernetes/kubeinvaders-deployment.yml -n kubeinvaders
 kubectl expose deployment  kubeinvaders --type=NodePort --name=kubeinvaders -n kubeinvaders --port 8080
 kubectl apply -f kubernetes/kubeinvaders-ingress.yml -n kubeinvaders
-kubectl create sa kubeinvaders -n foobar 
+kubectl create sa kubeinvaders -n foobar
+kubectl apply -f kubernetes/kubeinvaders-role.yml
+kubectl apply -f kubernetes/kubeinvaders-rolebinding.yml
+
 TOKEN=`kubectl describe secret $(kubectl get secret -n foobar | grep 'kubeinvaders-token' | awk '{ print $1}') -n foobar | grep 'token:' | awk '{ print $2}'`
-kubectl apply -f kubeinvaders-role.yml
+
 kubectl set env deployment/kubeinvaders TOKEN=$TOKEN -n kubeinvaders
 kubectl set env deployment/kubeinvaders NAMESPACE=$TARGET_NAMESPACE -n kubeinvaders
 kubectl set env deployment/kubeinvaders ROUTE_HOST=$ROUTE_HOST -n kubeinvaders

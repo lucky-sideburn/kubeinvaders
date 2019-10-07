bd6fd6296d4db8c3050b23e3b1f6e69d9b8a37a5
luckysideburn <eugenio.marzo@yahoo.it>
2019-10-07 15:54:58 -0400

    added vars in temporary_hack.sh

--- temporary_hack.sh
+++ temporary_hack.sh
@@ -2,7 +2,7 @@
 
 ENDPOINT=$(echo "https://${ROUTE_HOST}" | sed "s/\//\\\\\//g")
 
-sed -i "s/___buildEnvironment.called=true/___buildEnvironment.called=true;ENV[\"HIT_CPU_TIME_RATE_LIMIT\"]=\"$hit_cpu_time_rate_limit\";ENV[\"TOKEN\"]=\"$TOKEN\";ENV[\"ENDPOINT\"]=\"$ENDPOINT\/kube\";ENV[\"NAMESPACE\"]=\"$NAMESPACE\"/g" /var/www/html/KubeInvaders_wasm.js
+sed -i "s/___buildEnvironment.called=true/___buildEnvironment.called=true;ENV[\"TOKEN\"]=\"$TOKEN\";ENV[\"ENDPOINT\"]=\"$ENDPOINT\/kube\";ENV[\"NAMESPACE\"]=\"$NAMESPACE\";ENV[\"HITSLIMIT\"]=\"$HITSLIMIT\";ENV[\"ALIENPROXIMITY\"]=\"$ALIENPROXIMITY\\"/g" /var/www/html/KubeInvaders_wasm.js
 
 sed -i "s/TOTAL_ENV_SIZE=1024/TOTAL_ENV_SIZE=2048/g" /var/www/html/KubeInvaders_wasm.js
 sed -i "s/TOTAL_ENV_SIZE=1024/TOTAL_ENV_SIZE=2048/g" /var/www/html/KubeInvaders_asmjs.js

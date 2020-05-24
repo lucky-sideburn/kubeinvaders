32337818d8a7dd972535e245353768a4b7873f7e
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2020-05-24 08:28:44 +0200

    changed Kubeinvaders UI

--- main/mymodules/help.lua
+++ main/mymodules/help.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.get_help()
-  msg.post("ui#gui","help",{ position = vmath.vector3(856, 95, 0) })
+  msg.post("ui#gui","help",{ position = vmath.vector3(5, 300, 0) })
 end
 
 function M.quit_help()
-  msg.post("ui#gui","quit_help",{ position = vmath.vector3(-160, 800, 0) })
+  msg.post("ui#gui","quit_help",{ position = vmath.vector3(-1000, -1000, 0) })
 end
 
 return M

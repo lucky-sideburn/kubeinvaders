283f440da5523eaf7554db436b13d227a8be4cfd
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2021-01-03 08:43:46 +0100

    fix nginx

--- kube-linter/kube-linter-parser.sh
+++ kube-linter/kube-linter-parser.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-kube-linter lint $1 2>&1 |
-while read -r line
-do
-   echo
-   echo $line  |sed  -n 's/^\([^\ ]*\)\ \(([^\(]*)\)\ \(.*\)$/\3/p'
-   echo
-done
+POD_FILE=/tmp/${3}.yaml
+
+#Getting yaml of pod ${3} through kubernetes API
+curl -XGET "{$1}/api/v1/namespaces/{$2}/pods/{$3}" --header "Authorization: Bearer ${4}" --silent -k > ${POD_FILE}
+[ ! $? -eq 0 ] && (echo "{}" && exit 0)
+
+#Execute kubelinter against ${POD_FILE}
+wcl=$(cat ${POD_FILE} | wc -l )
+
+if [[ $wcl -gt 0 ]];then
+  kube-linter lint ${POD_FILE} 2>&1 |
+    while read -r line
+    do
+      echo $line | sed  -n 's/^\([^\ ]*\)\ \(([^\(]*)\)\ \(.*\)$/\3/p'
+    done | jq -R -s -c 'split("\n")'
+else
+  echo "{}"
+fi
+
+

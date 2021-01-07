115c5dacc6506a43b16c2f4d991b51cd6a9c5f9a
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2021-01-07 18:49:37 +0100

    fix chaos node

--- chaos-node/chaos-node.sh
+++ chaos-node/chaos-node.sh
@@ -10,7 +10,8 @@ YAML_FILE=/tmp/kubeinvaders-chaos-node-$RAND.yaml
 # Very temporary work-around
 KUBE=$(cat /etc/nginx/conf.d/KubeInvaders.conf | grep proxy_pass | head -n1 | awk '{ print $2 }' | sed 's/;//g')
 
-echo "$TOKEN $KUBE $RAND $NODE_NAME" > /tmp/foo
+# Only for debugging...
+# echo "$TOKEN $KUBE $RAND $NODE_NAME" > /tmp/foo
 
 cat <<EOF >$YAML_FILE
 apiVersion: batch/v1
@@ -38,7 +39,7 @@ sed -i  "s/random_suffix/$RAND/g" $YAML_FILE
 
 kubectl create -f $YAML_FILE --token=${TOKEN} --server=${KUBE}  -n ${NAMESPACE} --insecure-skip-tls-verify=true >> /tmp/kubectl.log 2>&1
 
-kubectl delete job -l app=kubeinvaders -l approle=chaosnode -o=jsonpath='{.items[?(@.status.succeeded==1)].metadata.name}' --token=${TOKEN} --server=${KUBE} -n $NAMESPACE >> /tmp/kubectl.log 2>&1
-kubectl delete pod --token=${TOKEN} --server=${KUBE} -l app=kubeinvaders -l approle=chaosnode --field-selector=status.phase==Succeeded -n $NAMESPACE >> /tmp/kubectl.log 2>&1
+kubectl delete job -l app=kubeinvaders -l approle=chaosnode -o=jsonpath='{.items[?(@.status.succeeded==1)].metadata.name}' --token=${TOKEN} --server=${KUBE} --insecure-skip-tls-verify=true -n $NAMESPACE >> /tmp/kubectl.log 2>&1
+kubectl delete pod --token=${TOKEN} --server=${KUBE} --insecure-skip-tls-verify=true -l app=kubeinvaders -l approle=chaosnode --field-selector=status.phase==Succeeded -n $NAMESPACE >> /tmp/kubectl.log 2>&1
 
 rm -f $YAML_FILE

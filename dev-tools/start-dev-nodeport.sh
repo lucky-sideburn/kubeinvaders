4f6feff1bd50199f3cbaefff9430f952680f4093
Eugenio Marzo <eugenio.marzo@yahoo.it>
2021-07-04 11:45:38 +0200

    Added support for NodePort service on Helm chart

--- dev-tools/start-dev-nodeport.sh
+++ dev-tools/start-dev-nodeport.sh
@@ -4,5 +4,5 @@ docker build . -t docker.io/luckysideburn/kubeinvaders:develop
 docker push luckysideburn/kubeinvaders:develop
 kubectl create namespace kubeinvaders
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-helm upgrade kubeinvaders --set-string target_namespace="namespace1\,namespace2" -n kubeinvaders ./helm-charts/kubeinvaders/ --set ingress.hostName=kubeinvaders.io --set image.tag=develop -i
+helm upgrade kubeinvaders --set-string target_namespace="namespace1\,namespace2" -n kubeinvaders ./helm-charts/kubeinvaders/ --set service.type="NodePort" --set ingress.hostName=kubeinvaders.io --set image.tag=develop -i
 kubectl delete pod --all -n kubeinvaders

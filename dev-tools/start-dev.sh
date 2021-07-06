6c33e9c2a058514f58876c2ff5e17e47f45bdec6
Eugenio Marzo <eugenio.marzo@yahoo.it>
2021-07-06 09:10:12 +0200

    changed dev start script

--- dev-tools/start-dev.sh
+++ dev-tools/start-dev.sh
@@ -5,4 +5,4 @@ docker push luckysideburn/kubeinvaders:develop
 kubectl create namespace kubeinvaders
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 helm upgrade kubeinvaders --set-string target_namespace="namespace1\,namespace2" -n kubeinvaders ./helm-charts/kubeinvaders/ --set ingress.hostName=kubeinvaders.io --set image.tag=develop -i
-kubectl delete pod --all -n kubeinvaders
+kubectl delete pod --force --grace-period=0 --all -n kubeinvaders

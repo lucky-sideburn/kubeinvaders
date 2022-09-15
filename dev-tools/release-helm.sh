dc6462bf17400cc421a4151bfdd306e723ea3d3a
Luckysideburn <luckysideburn@gmail.com>
2022-09-15 10:25:25 +0000

    fix release helm script

--- dev-tools/release-helm.sh
+++ dev-tools/release-helm.sh
@@ -1,4 +1,9 @@
-#Example...
-helm package .
-cp foo.tgz helm-charts/
-helm repo index.
+#! /bin/bash
+
+echo "Release new version of the KubeInvaders Helm Chart"
+
+helm package ../helm-charts/kubeinvaders/
+tar_gz_name=$(ls -art | grep "kubeinvaders.*.tgz")
+cp $tar_gz_name ../../helm-charts/
+cd ../../helm-charts/
+helm repo index .

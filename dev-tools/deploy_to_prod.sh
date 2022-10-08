43f3df70ec0178205bd1292f21944a89f9a240e8
Luckysideburn <luckysideburn@gmail.com>
2022-10-08 19:07:35 +0000

    some fix

--- dev-tools/deploy_to_prod.sh
+++ dev-tools/deploy_to_prod.sh
@@ -1,4 +1,6 @@
-docker build . -t docker.io/luckysideburn/kubeinvaders:$1
-docker tag docker.io/luckysideburn/kubeinvaders:$1 docker.io/luckysideburn/kubeinvaders:latest
-docker push docker.io/luckysideburn/kubeinvaders:$1
+image_version=$(cat ./README.md | grep 'image.tag' | awk -F= '{ print $4 }')
+echo $image_version
+docker build . -t docker.io/luckysideburn/kubeinvaders:$image_version
+docker tag docker.io/luckysideburn/kubeinvaders:$image_version docker.io/luckysideburn/kubeinvaders:latest
+docker push docker.io/luckysideburn/kubeinvaders:$image_version
 docker push docker.io/luckysideburn/kubeinvaders:latest
--- dev-tools/start_dev.sh
+++ dev-tools/start_dev.sh
@@ -3,6 +3,7 @@
 docker build . -t docker.io/luckysideburn/kubeinvaders:develop
 docker push luckysideburn/kubeinvaders:develop
 kubectl create namespace kubeinvaders
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-helm upgrade kubeinvaders --set-string target_namespace="namespace1\,namespace2" -n kubeinvaders ./helm-charts/kubeinvaders/ --set ingress.hostName=kubeinvaders.io --set image.tag=develop -i
+#export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+helm delete kubeinvaders -n kubeinvaders
+helm install kubeinvaders --set-string config.target_namespace="namespace1\,namespace2" -n kubeinvaders ./helm-charts/kubeinvaders/ --set ingress.enabled=true --set ingress.hostName=kubeinvaders.io --set deployment.image.tag=develop
 kubectl delete pod --force --grace-period=0 --all -n kubeinvaders

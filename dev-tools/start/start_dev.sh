11703ce3db94df3b8a01a70d292aa654f72b0adb
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2023-11-12 10:26:20 +0100

    clean dev tools

--- dev-tools/start/start_dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl create namespace kubeinvaders
-helm delete kubeinvaders -n kubeinvaders
-helm install kubeinvaders --set-string config.target_namespace="namespace1\,namespace2" -n kubeinvaders ./helm-charts/kubeinvaders/ --set ingress.enabled=true --set ingress.hostName=kubeinvaders.io --set deployment.image.tag=develop
-kubectl delete pod --force --grace-period=0 --all -n kubeinvaders
-
--- dev-tools/start/start_dev_nodeport.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build . -t docker.io/luckysideburn/kubeinvaders:develop
-docker push luckysideburn/kubeinvaders:develop
-kubectl create namespace kubeinvaders
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-helm upgrade kubeinvaders --set-string target_namespace="namespace1\,namespace2" -n kubeinvaders ./helm-charts/kubeinvaders/ --set service.type="NodePort" --set route_host="kubeinvaders.io" --set ingress.enabled="false" --set image.tag=develop -i
-kubectl delete pod --all -n kubeinvaders

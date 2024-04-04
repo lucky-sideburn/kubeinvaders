0bcb9537deaf0fefe557f0452aea3f5e6cfa1d9a
luckysideburn <luckysideburn@gmail.com>
2024-04-04 15:43:38 +0000

    foobar

--- dev-tools/push/push_prod_image.sh
+++ dev-tools/push/push_prod_image.sh
@@ -1,6 +1,8 @@
-image_version=$(cat ./README.md | grep 'image.tag' | awk -F= '{ print $4 }')
+image_version="v1.9.6"
 echo $image_version
-podman build . -t docker.io/luckysideburn/kubeinvaders:$image_version
+
+#podman build . -t docker.io/luckysideburn/kubeinvaders:$image_version
+podman tag local/kubeinvaders:develop docker.io/luckysideburn/kubeinvaders:$image_version 
 podman tag docker.io/luckysideburn/kubeinvaders:$image_version docker.io/luckysideburn/kubeinvaders:latest
 podman tag docker.io/luckysideburn/kubeinvaders:$image_version docker.io/luckysideburn/kubeinvaders:develop
 podman push docker.io/luckysideburn/kubeinvaders:$image_version

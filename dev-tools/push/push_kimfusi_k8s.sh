0e56bd0ab6f06ed9d8ad98447e200759b85f6e88
luckysideburn <luckysideburn@gmail.com>
2024-04-06 18:20:00 +0000

    fix helm chart

---
+++ dev-tools/push/push_kimfusi_k8s.sh
@@ -0,0 +1,2 @@
+podman tag local/kubeinvaders:develop 10.10.10.3:5000/kubeinvaders:v1.9.6
+podman push 10.10.10.3:5000/kubeinvaders:v1.9.6
--- dev-tools/push/push_prod_image.sh
+++ dev-tools/push/push_prod_image.sh
@@ -1,9 +1,14 @@
-image_version="v1.9.6"
+image_version="v1.9.7"
 echo $image_version
 
-#podman build . -t docker.io/luckysideburn/kubeinvaders:$image_version
+podman build . -t local/kubeinvaders:develop
+
 podman tag local/kubeinvaders:develop docker.io/luckysideburn/kubeinvaders:$image_version 
 podman tag docker.io/luckysideburn/kubeinvaders:$image_version docker.io/luckysideburn/kubeinvaders:latest
 podman tag docker.io/luckysideburn/kubeinvaders:$image_version docker.io/luckysideburn/kubeinvaders:develop
+podman tag docker.io/luckysideburn/kubeinvaders:$image_version 10.10.10.3:5000/kubeinvaders:v1.9.7
+
+
 podman push docker.io/luckysideburn/kubeinvaders:$image_version
 podman push docker.io/luckysideburn/kubeinvaders:latest
+podman push 10.10.10.3:5000/kubeinvaders:$image_version
